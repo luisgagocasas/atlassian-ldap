4bbe8e07b1b051035408d696ab8f13877de75cad
Luis Gago <luisgago@lagc-peru.com>
2021-06-03 18:39:38 -0500

    network | change network and apply phplapadmin

--- certgen.sh
+++ certgen.sh
@@ -2,3 +2,4 @@
 mkdir -p certs && cd $_
 mkcert -install
 mkcert -key-file jira.everis.local.key -cert-file jira.everis.local.crt jira.everis.local *.jira.everis.local
+mkcert -key-file ldap.everis.local.key -cert-file ldap.everis.local.crt ldap.everis.local *.ldap.everis.local
