737cfb4ca482b3d8dab1751bf5b48c0a918f9e71
Luis Gago <luisgago@lagc-peru.com>
2021-06-10 16:46:00 -0500

    insert | phpmyadmin

--- certgen.sh
+++ certgen.sh
@@ -4,3 +4,4 @@ mkcert -install
 mkcert -key-file jira.everis.local.key -cert-file jira.everis.local.crt jira.everis.local *.jira.everis.local
 mkcert -key-file ldap.everis.local.key -cert-file ldap.everis.local.crt ldap.everis.local *.ldap.everis.local
 mkcert -key-file wordpress.everis.local.key -cert-file wordpress.everis.local.crt wordpress.everis.local *.wordpress.everis.local
+mkcert -key-file pma.everis.local.key -cert-file pma.everis.local.crt pma.everis.local *.pma.everis.local
